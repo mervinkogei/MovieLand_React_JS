67c3c1b196bdf89c801860345aecde8e51872288
mervinkogei <vincent.kogei@yahoo.com>
2022-10-01 01:11:04 +0300

    movie test demo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import SearchIcon from "./search.svg";
+import MovieCard from './MovieCard';
 
-function App() {
+//Api Movie d492cd55
+const API_KEY = 'https://www.omdbapi.com?apikey=d492cd55'
+
+const movie1 = {
+  "Title": "Italian Spiderman",
+  "Year": "2007",
+  "imdbID": "tt2705436",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+}
+
+const App = () =>{
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_KEY}&s=${title}`);
+    const data = await response.json();
+    setMovies (data.Search)
+
+  }  
+
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+     <div className='search'>
+      <input placeholder='Search for Movies'
+      value={searchTerm}
+      onChange = {(e)=>{setSearchTerm(e.target.value)}}/>
+      <img
+      src={SearchIcon}
+      alt ='Search'
+      />
+     </div>
+     {
+      movies?.length > 0 ? ( <div className='container'>
+      {movies.map((movie) =>(
+        <MovieCard movie={movie}/>
+      ))}
+    </div>) : (
+      <div className='empty'>
+        <h2>No Movies Found</h2>
+      </div>
+    )
+     }
+    
     </div>
   );
 }
